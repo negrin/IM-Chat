5c1796d4b7743c99678b6323e6ea164e68de9c9e
Einat Bertenthal <einat.bertenthal@autodesk.com>
2017-06-04 16:04:42 +0300

    css fixes

--- app/components/comment.js
+++ app/components/comment.js
@@ -36,16 +36,17 @@ class Comment extends React.Component {
                         { this.props.comment.name } <span className="comment-command-description">{ this.props.comment.description }</span>
                     </div>
                     <div className="comment-text">
-                        { this.props.comment.showText && <div>{ this._handleBRintext() }</div> }
-                        { this.props.comment.videoInfo && this.props.comment.videoInfo.videoId &&
-                        <div className="comment-video-item">
+                        { this.props.comment.showText ?  this._handleBRintext() : null }
+                        { this.props.comment.videoInfo && this.props.comment.videoInfo.videoId
+                        ? <div className="comment-video-item">
                             <a href={ `https://www.youtube.com/watch?v=${ this.props.comment.videoInfo.videoId} ` } target="_blank"><img className="img"
                                  src={ `https://i.ytimg.com/vi/${ this.props.comment.videoInfo.videoId }/hqdefault.jpg` }/></a>
                             <div className="info">
                                 <div><a href={ `https://www.youtube.com/watch?v=${ this.props.comment.videoInfo.videoId} ` } target="_blank">{ this.props.comment.videoInfo.title }</a></div>
                                 <div>{ getVideoDuration(this.props.comment.videoInfo.duration) }</div>
                             </div>
-                        </div>
+                         </div>
+                        : null
                         }
                     </div>
                 </div>
